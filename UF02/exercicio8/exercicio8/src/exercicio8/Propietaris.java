b62eea099d838490044557cd7907b2c3bdde0e5c
JesusGargallo <71832344+JesusGargallo@users.noreply.github.com>
2020-11-20 20:09:50 +0100

    Update Propietaris.java

--- UF02/exercicio8/exercicio8/src/exercicio8/Propietaris.java
+++ UF02/exercicio8/exercicio8/src/exercicio8/Propietaris.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio8;
 
-/**
- *
- * @author Usuario
- */
-public class Propietaris {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Propietaris implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id @GeneratedValue
+    private long id;
+
+    private int añosdecarnet;
+    private String nombre;
+    private Date nacimiento;
+    private boolean isdiscapacitado;
+
+    public Propietaris(){
+    }
+
+    Propietaris(String nombre, int añosdecarnet, Date nacimiento, boolean isdiscapacitado){
+        this.nombre = nombre;
+        this.añosdecarnet = añosdecarnet;
+        this.nacimiento = nacimiento;
+        this.isdiscapacitado = isdiscapacitado;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getAñosdeCarnet(){
+        return añosdecarnet;
+    }
+
+    public Date getNacimiento(){
+        return nacimiento;
+    }
+
+    public boolean getIsDiscapacitado(){
+        return isdiscapacitado;
+    }
+
+    @Override
+    public String toString() {
+        return "Propietaris{" + "id=" + id + 
+                ", a\u00f1osdecarnet=" + añosdecarnet + 
+                ", nombre=" + nombre + ", nacimiento=" + nacimiento + 
+                ", isdiscapacitado=" + isdiscapacitado + '}';
+    }
+
     
+
+
 }
